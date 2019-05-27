61a52aac0c868f19c3f5d349815a2237e7b1df87
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-27 17:47:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,11 +61,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash.map do |name, language|
-     if name == :dennis_ritchie
-     return language.
-     end
-     end 
+     programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
